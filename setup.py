fb0c429579890a81ff21a9358cd743e5ac77ec00
ChipMcCallahan <thisischipmccallahan@gmail.com>
2022-08-30 18:56:41 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='cc1_tools',
+    url='https://github.com/ChipMcCallahan/CC1Tools',
+    author='Chip McCallahan',
+    author_email='thisischipmccallahan@gmail.com',
+    # Needed to actually package something
+    packages=find_packages(),
+    # Needed for dependencies
+    install_requires=['protobuf'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='LICENSE',
+    description='Assorted tools for working with and displaying Chip\'s Challenge levels in DAT file format.',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
